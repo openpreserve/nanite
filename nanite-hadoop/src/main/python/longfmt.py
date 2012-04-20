94d834ff7326d1500cab15cad0fe783bfca7bacb
Andrew Jackson <Andrew.Jackson@bl.uk>
2012-04-20 22:42:33 +0100

    Added some code for summing totals, but not quite right.

--- nanite-hadoop/src/main/python/longfmt.py
+++ nanite-hadoop/src/main/python/longfmt.py
@@ -12,6 +12,25 @@ for row in tsv_file:
     (fmtS, fmtT, fmtD, year, count) = row
     fmt = bestType(fmtS,fmtT,fmtD)
     fmt = reduceType(fmt,True)
+    
+    # This is a temporary override that looks at other stuff:
+    # ---
+    try:
+        if fmtT != "null":
+            (type, subtype, params) = mimeparse.parse_mime_type(fmtT)
+    except:
+        print "ERROR: Could not parse: "+fmtT
+        exit
+    key = 'producer'
+    if params.has_key(key):
+        fmt = params[key]
+        fmt = re.sub(r"(\d+)\.\d.*",r"\1",fmt)
+    else:
+        fmt = None 
+    #if params.has_key("version"):
+    #    fmt = "{} {}".format(fmt,params["version"])
+    # ---
+    # 
 
     if not fmt in byy:
         byy[fmt] = {}
@@ -32,8 +51,16 @@ print out
 for fmt in sorted(byy):
     out = fmt
     for year in range(minYear,maxYear+1):
+        
+        # Calculate total:
+        total = 0
+        for fmt2 in byy:
+            if byy[fmt2].has_key(year):
+                total += byy[fmt2][year]
+                
+        # Output the values:
         if byy[fmt].has_key(year):
-            out = "{}\t{}".format(out,byy[fmt][year])
+            out = "{}\t{}".format(out,100.0*float(byy[fmt][year])/float(total))
         else:
             out = "{}\t{}".format(out,0)
     print out
