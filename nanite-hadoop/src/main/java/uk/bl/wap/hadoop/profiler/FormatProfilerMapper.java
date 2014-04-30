bc5c70642aa910b4cb28085fc2855b9577da1e4e
pmd <pmd@statsbiblioteket.dk>
2014-04-30 21:00:57 +0200

    Replace all newlines with spaces in extracted metadata

--- nanite-hadoop/src/main/java/uk/bl/wap/hadoop/profiler/FormatProfilerMapper.java
+++ nanite-hadoop/src/main/java/uk/bl/wap/hadoop/profiler/FormatProfilerMapper.java
@@ -360,7 +360,7 @@ public class FormatProfilerMapper extends MapReduceBase implements Mapper<Text, 
 		String[] names = metadata.names();
 		for(String name : names) {
 			for(String value : metadata.getValues(name)) {
-				pw.println(name+": "+value.replace("\n"," ");
+				pw.println(name+": "+value.replace("\n"," "));
 			}
 		}
 		
