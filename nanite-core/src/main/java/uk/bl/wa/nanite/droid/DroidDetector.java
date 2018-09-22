4d58d8354dd5a1cb6c9eee19d53fdfc9691c200e
Andrew Jackson <Andrew.Jackson@bl.uk>
2018-09-22 19:20:16 +0100

    Split old sigs out into separate module, update container sig.

--- nanite-core/src/main/java/uk/bl/wa/nanite/droid/DroidDetector.java
+++ nanite-core/src/main/java/uk/bl/wa/nanite/droid/DroidDetector.java
@@ -121,7 +121,7 @@ public class DroidDetector implements Detector {
 	static final String DROID_SIG_RESOURCE = "droid/" + DROID_SIGNATURE_FILE;
 
 	static final String DROID_SIG_FILE = "" + DROID_SIG_RESOURCE;
-    static final String CONTAINER_SIG_FILE = "droid/container-signature-20180917.xml";
+    static final String CONTAINER_SIG_FILE = "droid/container-signature-20180920.xml";
 
 	// Set up DROID binary handler:
 	private BinarySignatureIdentifier binarySignatureIdentifier;
